8b491d07369ec6121d4a83150d069e040f6577f3
Cedric Vangout <mail@cvng.dev>
2023-11-26 02:37:24 +0400

    feat: basic stream

--- tests/test_printers_prettification.rs
+++ tests/test_printers_prettification.rs
@@ -3,7 +3,33 @@
 use pg_deparser::stream::IndentedStream;
 
 const PASSES: [&str; 1] = [
-    "tests/test_printers_prettification/ddl/create_domain.sql", //
+    // "tests/test_printers_prettification/ddl/alter_default_privileges.sql",
+    // "tests/test_printers_prettification/ddl/alter_subscription.sql",
+    // "tests/test_printers_prettification/ddl/alter_table.sql",
+    // "tests/test_printers_prettification/ddl/alter_text_search.sql",
+    // "tests/test_printers_prettification/ddl/comment.sql",
+    // "tests/test_printers_prettification/ddl/create_aggregate.sql",
+    // "tests/test_printers_prettification/ddl/create_database.sql",
+    // "tests/test_printers_prettification/ddl/create_domain.sql",
+    // "tests/test_printers_prettification/ddl/create_event_trigger.sql",
+    // "tests/test_printers_prettification/ddl/create_extension.sql",
+    // "tests/test_printers_prettification/ddl/create_foreign_data_wrapper.sql",
+    // "tests/test_printers_prettification/ddl/create_foreign_table.sql",
+    // "tests/test_printers_prettification/ddl/create_function.sql",
+    // "tests/test_printers_prettification/ddl/create_index.sql",
+    // "tests/test_printers_prettification/ddl/create_language.sql",
+    // "tests/test_printers_prettification/ddl/create_rule.sql",
+    // "tests/test_printers_prettification/ddl/create_schema.sql",
+    // "tests/test_printers_prettification/ddl/create_sequence.sql",
+    // "tests/test_printers_prettification/ddl/create_table.sql",
+    // "tests/test_printers_prettification/ddl/create_transform.sql",
+    // "tests/test_printers_prettification/ddl/create_trigger.sql",
+    "tests/test_printers_prettification/ddl/create_type.sql",
+    // "tests/test_printers_prettification/ddl/create_view.sql",
+    // "tests/test_printers_prettification/ddl/grant.sql",
+    // "tests/test_printers_prettification/ddl/issue110.sql",
+    // "tests/test_printers_prettification/ddl/notify.sql",
+    // "tests/test_printers_prettification/ddl/show.sql",
 ];
 
 #[test]
