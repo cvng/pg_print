663d8ebc19165497123d0d1de2aae1dc1e832cf1
Cedric Vangout <mail@cvng.dev>
2023-11-30 00:33:41 +0400

    val

--- src/val.rs
+++ src/val.rs
@@ -2,14 +2,9 @@ use crate::fmt;
 use crate::utils::deparse_string_literal;
 use pg_query::protobuf::a_const::Val;
 
-impl fmt::Print for Option<Val> {
+impl fmt::Print for Val {
     fn print_in_context(&self, p: &mut fmt::Printer, ctx: &fmt::Context) -> fmt::Option {
-        let Some(val) = self else {
-            p.keyword("null");
-            return Some(());
-        };
-
-        match val {
+        match self {
             Val::Ival(val) => p.word(format!("{}", val.ival)),
             Val::Fval(val) => p.word(val.fval.clone()),
             Val::Boolval(val) => p.word(if val.boolval { "true" } else { "false" }),
@@ -32,5 +27,17 @@ impl fmt::Print for Option<Val> {
         }
 
         Some(())
+    }
+}
+
+impl fmt::Print for Option<Val> {
+    fn print_in_context(&self, p: &mut fmt::Printer, ctx: &fmt::Context) -> fmt::Option {
+        match self {
+            Some(val) => val.print_in_context(p, ctx),
+            None => {
+                p.keyword("null");
+                Some(())
+            }
+        }
     }
 }
