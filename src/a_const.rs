7bf5ff21ea0860b79217ec230f4c1bcf760c332f
Cedric Vangout <mail@cvng.dev>
2023-11-30 02:40:56 +0400

    print

--- src/a_const.rs
+++ src/a_const.rs
@@ -1,9 +1,8 @@
 use crate::fmt;
-use pg_query::protobuf::a_const::Val;
 use pg_query::protobuf::AConst;
 
 impl fmt::Print for AConst {
     fn print(&self, p: &mut fmt::Printer) -> fmt::Option {
-        Option::<Val>::print_in_context(&self.val, p, &fmt::Context::Constant)
+        self.val.print_in_context(p, &fmt::Context::Constant)
     }
 }
--- src/integer.rs
+++ src/integer.rs
@@ -4,6 +4,6 @@ use pg_query::protobuf::Integer;
 
 impl fmt::Print for Integer {
     fn print(&self, p: &mut fmt::Printer) -> fmt::Option {
-        Option::<Val>::print(&Some(Val::Ival(self.clone())), p)
+        Some(Val::Ival(self.clone())).print(p)
     }
 }
