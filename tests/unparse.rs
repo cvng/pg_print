ae747dbbbbc34a452aabde6cd72dccbf83faab31
Cedric Vangout <mail@cvng.dev>
2023-12-15 22:11:48 +0100

    tests

--- tests/unparse.rs
+++ tests/unparse.rs
@@ -2,37 +2,27 @@ use insta::assert_snapshot;
 use parser::parse_source;
 use pg_query::parse;
 use std::fs;
-use std::path::PathBuf;
+use std::path::Path;
 
 #[test]
 fn unparse() {
-    env_logger::builder().is_test(true).try_init().unwrap();
+    let path = Path::new("tests/unparse.sql");
 
-    let path = PathBuf::from("tests/unparse.sql");
-
-    for (mut lineno, case) in fs::read_to_string(&path).unwrap().lines().enumerate() {
-        lineno += 1;
+    for (mut line, case) in fs::read_to_string(path).unwrap().lines().enumerate() {
+        line += 1;
 
         if case.starts_with("--") {
             continue;
         }
 
-        let deparsed = pg_query::deparse(&parse(case).unwrap().protobuf)
-            .unwrap()
-            .to_string();
-
         let unparsed = pg_print::unparse(&parse_source(case));
+        let deparsed = pg_query::deparse(&parse(case).unwrap().protobuf).unwrap();
+        let reparsed = pg_query::deparse(&parse(&unparsed).unwrap().protobuf).unwrap();
 
-        let reparsed = pg_query::deparse(&parse(&unparsed).unwrap().protobuf)
-            .unwrap()
-            .to_string();
-
-        let fingerprint = pg_query::fingerprint(case).unwrap().hex.to_string();
-
-        let description = format!("{}:{}", path.display(), lineno);
+        let fingerprint = pg_query::fingerprint(case).unwrap();
+        let description = format!("{}:{}", path.display(), line);
 
         assert_eq!(deparsed, reparsed, "{}", &description);
-
-        assert_snapshot!(fingerprint, unparsed, &description);
+        assert_snapshot!(fingerprint.hex, unparsed, &description);
     }
 }
