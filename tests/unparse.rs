897b6b515911f276621bc84fddbe8733dfc78777
Cedric Vangout <mail@cvng.dev>
2023-12-24 17:14:33 +0100

    bump

--- tests/unparse.rs
+++ tests/unparse.rs
@@ -1,15 +1,14 @@
 use insta::assert_snapshot;
-use pg_query::parse;
 use std::fs;
 use std::path::Path;
 
 #[cfg(feature = "unstable")]
-fn parse_source(text: &str) -> parser::Parse {
+fn parse(text: &str) -> parser::Parse {
     parser::parse_source(text)
 }
 
 #[cfg(not(feature = "unstable"))]
-fn parse_source(text: &str) -> pg_query::protobuf::ParseResult {
+fn parse(text: &str) -> pg_query::protobuf::ParseResult {
     pg_query::parse(text).unwrap().protobuf
 }
 
@@ -24,9 +23,9 @@ fn unparse() {
             continue;
         }
 
-        let unparsed = pg_print::unparse(&parse_source(case));
-        let deparsed = pg_query::deparse(&parse(case).unwrap().protobuf).unwrap();
-        let reparsed = pg_query::deparse(&parse(&unparsed).unwrap().protobuf).unwrap();
+        let unparsed = pg_print::unparse(&parse(case));
+        let deparsed = pg_query::deparse(&pg_query::parse(case).unwrap().protobuf).unwrap();
+        let reparsed = pg_query::deparse(&pg_query::parse(&unparsed).unwrap().protobuf).unwrap();
 
         let fingerprint = pg_query::fingerprint(case).unwrap().hex;
         let description = format!("{}:{}", path.display(), line);
